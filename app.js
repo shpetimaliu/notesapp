b4e71ea90882bb32aa4792cb2b1bab22d1b6d5eb
Shpetim <shpetimmaliu@gmail.com>
2023-07-22 00:01:16 +0200

    Start create auth

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +28,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 // Routers
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
