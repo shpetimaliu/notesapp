5db1850682a6ec444abefecc4e6b069d13226593
Shpetim <shpetimmaliu@gmail.com>
2023-07-22 02:25:42 +0200

    🛠️

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,15 +1,49 @@
 const Note = require("../models/Notes");
 const mongoose = require("mongoose");
 
-exports.dashboard = async (req, res) => {
+exports.dashboard = async (req, res, next) => {
+  let perPage = 12;
+  let page = req.query.page || 1;
   const locals = {
     title: "Dashboard",
     description: "NotesApp Build with NodeJS",
   };
 
-  res.render("dashboard/index", {
-    userName: req.user.firstName,
-    locals,
-    layout: "../views/layouts/dashboard",
-  });
+  try {
+    Note.aggregate([
+      {
+        $sort: {
+          createdAt: -1,
+        },
+      },
+      {
+        $match: { user: mongoose.Types.ObjectId(req.user.id) },
+      },
+      {
+        $project: {
+          title: { substr: ["$title", 0, 30] },
+          body: { substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec(function (err, note) {
+        Note.count().exec(function (err, count) {
+          if (err) return next(err);
+
+          res.render("dashboard/index", {
+            userName: req.user.firstName,
+            locals,
+            notes,
+            layout: "../views/layouts/dashboard",
+          });
+        });
+      });
+
+    const notes = await Note.find({});
+    console.log(notes);
+  } catch (error) {
+    console.log(error);
+  }
 };
