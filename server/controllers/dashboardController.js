691351d0e24a080e439f293970b7571fb0a74ea8
Shpetim <shpetimmaliu@gmail.com>
2023-07-22 02:41:55 +0200

    fix some bugs ⚙

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,49 +1,50 @@
 const Note = require("../models/Notes");
 const mongoose = require("mongoose");
 
-exports.dashboard = async (req, res, next) => {
-  let perPage = 12;
-  let page = req.query.page || 1;
-  const locals = {
-    title: "Dashboard",
-    description: "NotesApp Build with NodeJS",
-  };
+exports.dashboard = async (req, res) => {
+  const perPage = 12;
+  const page = req.query.page || 1;
 
   try {
-    Note.aggregate([
+    const notes = await Note.aggregate([
+      {
+        $match: { user: new mongoose.Types.ObjectId(req.user.id) },
+      },
       {
         $sort: {
           createdAt: -1,
         },
       },
       {
-        $match: { user: mongoose.Types.ObjectId(req.user.id) },
-      },
-      {
         $project: {
-          title: { substr: ["$title", 0, 30] },
-          body: { substr: ["$body", 0, 100] },
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
         },
       },
-    ])
-      .skip(perPage * page - perPage)
-      .limit(perPage)
-      .exec(function (err, note) {
-        Note.count().exec(function (err, count) {
-          if (err) return next(err);
+      {
+        $skip: perPage * page - perPage,
+      },
+      {
+        $limit: perPage,
+      },
+    ]);
 
-          res.render("dashboard/index", {
-            userName: req.user.firstName,
-            locals,
-            notes,
-            layout: "../views/layouts/dashboard",
-          });
-        });
-      });
+    const count = await Note.count();
 
-    const notes = await Note.find({});
-    console.log(notes);
+    res.render("dashboard/index", {
+      userName: req.user.firstName,
+      locals: {
+        title: "Dashboard",
+        description: "NotesApp Build with NodeJS",
+      },
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage),
+    });
   } catch (error) {
     console.log(error);
+    // Këtu mund të shtoni një përgjigje për gabimin në faqen e erroreve
+    res.status(500).send("Something went wrong...");
   }
 };
